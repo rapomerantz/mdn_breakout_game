4a3b3e6f735b3dc6832e6dd2dbded261e32e198d
Atticus Pomerantz <rapomerantz@gmail.com>
2018-06-24 17:19:59 -0500

    ball changes color randomly

--- scripts.js
+++ scripts.js
@@ -7,15 +7,18 @@ console.log('js');
 
     let x = canvas.width/2;
     let y = canvas.height-30;
+    let dx = 5;
+    let dy = -5;
 
-    let dx = 2;
-    let dy = -2;
+    let ballRadius = 25;
 
+    let ballFillColor = "#0095DD"
 
+    
     function drawBall() {
         ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI*2);
-        ctx.fillStyle = "#0095DD";
+        ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+        ctx.fillStyle = ballFillColor;
         ctx.fill();
         ctx.closePath();
     }
@@ -23,8 +26,23 @@ console.log('js');
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall();
+
+        if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+            dx = -dx;
+            ballFillColor = changeBallColor(); 
+        }
+        if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+            dy = -dy;
+            ballFillColor = changeBallColor(); 
+
+        }
+
         x += dx;
         y += dy;
+    }
+
+    function changeBallColor () {
+        return '#'+Math.floor(Math.random()*16777215).toString(16); 
     }
 
     setInterval(draw, 10); 
