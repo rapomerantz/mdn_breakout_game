6788ce5b9da794ec55c1f3b4a59c22efc7bccbf7
Atticus Pomerantz <rapomerantz@gmail.com>
2018-06-24 17:49:39 -0500

    Paddle collision working

--- scripts.js
+++ scripts.js
@@ -4,15 +4,22 @@ console.log('js');
     let ctx = canvas.getContext('2d'); 
 
 
-
+//DEFINE BALL & BALL MOVEMENT
     let x = canvas.width/2;
     let y = canvas.height-30;
     let dx = 5;
     let dy = -5;
-
     let ballRadius = 25;
-
     let ballFillColor = "#0095DD"
+//DEFINE PADDLE
+    let paddleHeight = 10;
+    let paddleWidth = 75;
+    let paddleX = (canvas.width-paddleWidth)/2;
+//PADDLE MOVEMENT
+    let rightPressed = false;
+    let leftPressed = false;
+
+    let counter = 0; 
 
     
     function drawBall() {
@@ -22,19 +29,49 @@ console.log('js');
         ctx.fill();
         ctx.closePath();
     }
+
+    function drawPaddle() {
+        ctx.beginPath();
+        ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
     
-    function draw() {
+    function draw() {        
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall();
+        drawPaddle(); 
 
+        //If ball its either side, dx is inverted
         if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
             dx = -dx;
             ballFillColor = changeBallColor(); 
         }
-        if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-            dy = -dy;
-            ballFillColor = changeBallColor(); 
 
+        //If ball hits top dy is inverted
+        if(y + dy < ballRadius) {
+            dy = -dy;
+        //else if ball hits paddle dy is inverted. 
+        } else if(y + dy > canvas.height-ballRadius) {
+            if(x > paddleX && x < paddleX + paddleWidth) {
+                dy = -dy;
+                counter ++; 
+                console.log(counter);
+                
+            }
+            //if ball doesn't hit paddle, game over
+            else {
+                alert("GAME OVER");
+                // document.location.reload();
+            }
+        }
+
+        if(rightPressed && paddleX < canvas.width-paddleWidth) {
+            paddleX += 7;
+        }
+        else if(leftPressed && paddleX > 0) {
+            paddleX -= 7;
         }
 
         x += dx;
@@ -44,5 +81,31 @@ console.log('js');
     function changeBallColor () {
         return '#'+Math.floor(Math.random()*16777215).toString(16); 
     }
+
+    document.addEventListener('keydown', keyDownHandler, false); 
+    document.addEventListener('keyup', keyUpHandler, false);
+
+    function keyDownHandler(e) {
+        if(e.keyCode == 39) {
+            rightPressed = true;
+        } else if(e.keyCode == 37) {
+            leftPressed = true;
+        } 
+        // else if (e.keyCode == 81) {
+        //     dx += Math.abs(dx) + 1;             
+        // } else if (e.keyCode == 65) {
+        //     dx -= Math.abs(dx) - 1; 
+        // }
+    }
+    
+    function keyUpHandler(e) {
+        if(e.keyCode == 39) {
+            rightPressed = false;
+        }
+        else if(e.keyCode == 37) {
+            leftPressed = false;
+        }
+    }
+
 
     setInterval(draw, 10); 
